d368f3994878a6ef5db96e42653e26d1a4d3eb24
Didem Nursal Köme <didem@galacticentertainment.com>
2025-06-29 18:07:48 +0300

    deploy

--- src/pages/CreateUser.tsx
+++ src/pages/CreateUser.tsx
@@ -39,8 +39,6 @@ export const CreateUser = () => {
     navigate(routes.USER_DETAILS);
   };
 
-  if (!defaultValues) return null;
-
   return (
     <div className="min-h-screen bg-gray-100 flex items-center justify-center px-4">
       <div className="bg-white p-8 rounded-lg shadow-md w-full max-w-md">
